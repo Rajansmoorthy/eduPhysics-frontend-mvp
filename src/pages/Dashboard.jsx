7f799a7a3022f1b8ed316ee7ba954e7c95282326
Rajansmoorthy <75314240+Rajansmoorthy@users.noreply.github.com>
2025-12-27 21:19:55 +0530

    feat: add student dashboard page

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,28 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h2>Student Dashboard</h2>
+
+      <label>
+        Select Class:
+        <select>
+          <option>Class 10</option>
+          <option>Class 11</option>
+          <option>Class 12</option>
+        </select>
+      </label>
+
+      <br /><br />
+
+      <label>
+        Select Topic:
+        <select>
+          <option>Electricity</option>
+          <option>Magnetism</option>
+          <option>Optics</option>
+          <option>Mechanics</option>
+        </select>
+      </label>
+    </div>
+  );
+}
